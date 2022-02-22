7a9c87bd7e832071eeae4abbd3c109bcc93c3daa
seongjun <reasonjun@daum.net>
2022-02-22 09:52:25 +0900

    move:

--- src/test/java/com/reasonjun/cinema/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/reasonjun/cinema/member/repository/MemberRepositoryTest.java
@@ -1,17 +1,15 @@
-package com.reasonjun.cinema.member;
+package com.reasonjun.cinema.member.repository;
 
 import com.reasonjun.cinema.member.domain.Member;
-import com.reasonjun.cinema.member.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
 class MemberRepositoryTest {
 
@@ -20,8 +18,7 @@ class MemberRepositoryTest {
 
     @Test
     void findAllTest() {
-        List<Member> members = repository.findAll();
-        assertThat(members.size()).isEqualTo(1);
+        assertThat(repository.findAll().size()).isEqualTo(1);
     }
 
     @Test
